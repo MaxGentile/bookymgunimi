dbda809c76e5527da5a941e7d694d75b07d10bcf
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-16 09:27:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ var http = require('http');
 var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
-const cors = require('cors');
-
-// Imposta la struttura delle cartelle
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-}
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(__dirname + '/public'));
 }) 
 // imposta la route
