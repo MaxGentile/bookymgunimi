583e6280dda1ba25213015fa625f6e71b68f7458
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-16 16:07:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,26 +3,25 @@
 var express = require('express');//abilita express
 var http = require('http');//abilita http
 var path = require('path');//abilita path
-
+var port = process.env.PORT || 3000;
 var app = express();
-var port = process.env.PORT
+
 // Imposta la struttura delle cartelle
 app.use(express.static(__dirname + '/public'));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://bookymgunimi.netsons.org/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 // imposta la route
 app.get('/', (req, res,next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-app.post('/', function(req, res, next) {
-    // Handle the post for this route
-   });
+ // Enable CORS
+app.use(function( req, res, next ) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Max-Age", "1000000000");
+  // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
 var server = http.createServer(app);
 server.listen(port);
 console.log("Il criceto gira sulla porta " + port);
 
-
+})
