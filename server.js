3cbfc3e6cce5b5d6df706d4739925266f4eedfef
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-16 16:10:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,15 @@ var app = express();
 // Imposta la struttura delle cartelle
 app.use(express.static(__dirname + '/public'));
 // imposta la route
+app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
 app.get('/', (req, res,next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
- // Enable CORS
-app.use(function( req, res, next ) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Max-Age", "1000000000");
-  // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
+ 
 var server = http.createServer(app);
 server.listen(port);
 console.log("Il criceto gira sulla porta " + port);
