ce83f2701328bfc5c63737f1f804403685a72fe2
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-16 09:40:37 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 // Server Node.js 
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express = require('express');//abilita express
+var http = require('http');//abilita http
+var path = require('path');//abilita path
 var port = process.env.PORT || 3000;
+var cors = require('cors'); //abilita cors
 var app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+// Imposta la struttura delle cartelle
 app.use(express.static(__dirname + '/public'));
-}) 
+app.use(cors());
 // imposta la route
 app.get('/', (req, res,next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
