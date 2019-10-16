c7d4e2370bb10eec80715dc4bcb5373e2d22eb5d
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-16 09:46:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var express = require('express');//abilita express
 var http = require('http');//abilita http
 var path = require('path');//abilita path
 var port = process.env.PORT || 3000;
-var cors = require('cors'); //abilita cors
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.get('/endpoint', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 // Imposta la struttura delle cartelle
 app.use(express.static(__dirname + '/public'));
-app.use(cors());
 // imposta la route
+
 app.get('/', (req, res,next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
