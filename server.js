17ed0f2b9c5df31068774d7fff24ea6b1ccf2856
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-17 11:34:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 // Server Node.js 
 
 var express = require('express');//abilita express
-var http = require('http');//abilita http
 var path = require('path');//abilita path
+var http = require('http');
 var port = process.env.PORT || 3000;
-var cors = require('cors'); //abilita cors
 var app = express();
 
-// Imposta la struttura delle cartelle
-app.use(express.static(__dirname + '/public'));
-app.use(cors());
-// imposta la route
-app.get('/', (req, res,next) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  if (process.env.NODE_ENV === 'production') {
+	app.use(express.static("public"));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
+
  
 var server = http.createServer(app);
 server.listen(port);
 console.log("Il criceto gira sulla porta " + port);
-
-
